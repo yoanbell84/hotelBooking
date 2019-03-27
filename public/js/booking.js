8b037fef9055f661562baad8545ec5bc3fea132b
yoabell <yoabell84@gmail.com>
2019-03-27 15:17:03 -0400

    added validation to booking hotel

---
+++ public/js/booking.js
@@ -0,0 +1,31 @@
+
+$(document).on('submit', 'form#frm', function (event) {
+    event.preventDefault();
+    var form = $(this);
+    var data = new FormData($(this)[0]);
+    var url = form.attr("action");
+    $.ajax({
+        type: form.attr('method'),
+        url: url,
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            $('.is-invalid').removeClass('is-invalid');
+            if (data.fail) {
+                for (control in data.errors) {
+                    $('input[name=' + control + ']').addClass('is-invalid');
+                    $('#error-' + control).html(data.errors[control]);
+                }
+            } else {
+                $('#modalForm').modal('hide');
+                ajaxLoad(data.redirect_url);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert("Error: " + errorThrown);
+        }
+    });
+    return false;
+});
---
+++ resources/views/front/forms/booking.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoan
+ * Date: 3/27/19
+ * Time: 1:32 PM
+ */
